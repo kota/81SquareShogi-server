93985bdc8726436896364c2cf8acda05fb1d86b6
Hidetchi <checkmate.1107@nifty.com>
2010-09-25 13:47:47 +0900

    対局リストの実装のため、%%LISTコマンドのレスポンスを充実

--- shogi_server/command.rb
+++ shogi_server/command.rb
@@ -739,7 +739,13 @@ module ShogiServer
     def call
       buf = Array::new
       @games.each do |id, game|
-        buf.push(sprintf("##[LIST] %s\n", id))
+        buf.push(sprintf("##[LIST] %s %d %d %d %d %d\n",
+                         id,
+                         game.current_turn,
+                         game.sente.rate,
+                         game.gote.rate,
+                         game.sente.country_code,
+                         game.gote.country_code))
       end
       buf.push("##[LIST] +OK\n")
       @player.write_safe(buf.join)
