164d9b6b4ef11aeb8bf1736f9401d63f714a5e12
Hidetchi <checkmate.1107@nifty.com>
2011-02-02 23:24:31 +0900

    Simplified code structure for population log.

--- shogi_server/server.rb
+++ shogi_server/server.rb
@@ -6,6 +6,18 @@ module ShogiServer # for a namespace
 class Server < ActiveResource::Base
   self.site = 'http://localhost:3000'
   self.prefix = '/api/'
+  
+  def start_log
+    self.started_at = Time.now
+    self.maintenance_at = nil
+    write_population(0)
+  end
+  
+  def write_population(n)
+    self.population = n
+    save
+  end
+
 end # class
 
 end # ShogiServer
